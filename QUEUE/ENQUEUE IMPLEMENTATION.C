7846818ea2c08a60ce009f82e5c9b43626aede4c
Srinithi K <srinithik0412@gmail.com>
2025-12-29 22:07:53 +0530

    Create ENQUEUE IMPLEMENTATION.C

---
+++ QUEUE/ENQUEUE IMPLEMENTATION.C
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node*next;
+};
+
+struct node*front = NULL;
+struct node*rear = NULL;
+
+int main()
+{
+   int n,value;
+   printf("Enter number of element to enqueue : ");
+   scanf("%d",&n);
+   
+   for(int i = 0 ; i < n ; i++)
+   {
+       struct node*newnode=malloc(sizeof(struct node));
+       scanf("%d",&value);
+       
+       newnode->data=value;
+       newnode->next=NULL;
+       if (rear==NULL){
+           front=rear=newnode;
+       }
+       else{
+           rear->next=newnode;
+           rear=newnode;
+       }
+   }
+   
+   printf("\nQueue stored in linked list\n");
+   struct node*temp=front;
+   while(temp!=NULL){
+       
+       printf("%d\n",temp->data);
+       temp=temp->next;
+   }
+   return 0;
+}

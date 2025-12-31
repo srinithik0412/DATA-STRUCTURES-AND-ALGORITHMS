ec0c04936321037e42923c180249712e4406341f
Srinithi K <srinithik0412@gmail.com>
2025-12-31 10:20:07 +0530

    Create TOP K ELEMENTS IMPLEMENTATION.C

---
+++ ARRAY/TOP K ELEMENTS IMPLEMENTATION.C
@@ -0,0 +1,32 @@
+//Using bubble sort
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter K: ");
+    scanf("%d", &k);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Top %d elements: ", k);
+    for (int i = 0; i < k; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
